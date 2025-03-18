25533e68660b55e7a3e75d8d01ba327ffff9d777
nicolaanghileri <nicola.anghileri@samtrevano.ch>
2025-03-18 14:21:18 +0100

    Last exercise on joining various threads

--- ad_ex_nan/src/main/java/ch/hslu/sw04/joins/JoinAndSleep.java
+++ ad_ex_nan/src/main/java/ch/hslu/sw04/joins/JoinAndSleep.java
@@ -1,5 +1,64 @@
 package ch.hslu.sw04.joins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JoinAndSleep - Demo class for Threads waiting for oder Threads.
+ *
+ * @author Nicola Anghileri - nicola.anghileri@stud.hslu.ch
+ * @version 17.03.2025
+ */
 public class JoinAndSleep {
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(JoinAndSleep.class);
+
+    /**
+     * Privater Konstruktor, da statische Demo-Klasse.
+     */
+    private JoinAndSleep() {
+        // nothing here
+    }
+
+    /**
+     * Main-Demo.
+     *
+     * @param args not used
+     * @throws InterruptedException falls das Warten (join) unterbrochen wird
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1) Erzeuge die Tasks
+        JoinAndSleepTask taskA = new JoinAndSleepTask("A", 2000);
+        JoinAndSleepTask taskB = new JoinAndSleepTask("B", 3000);
+        JoinAndSleepTask taskC = new JoinAndSleepTask("C", 4000);
+
+        // 2) Erzeuge die Threads, die diese Tasks ausführen
+        Thread threadA = new Thread(taskA, "ThreadA");
+        Thread threadB = new Thread(taskB, "ThreadB");
+        Thread threadC = new Thread(taskC, "ThreadC");
+
+        // 3) Konfiguriere Join-Beziehungen:
+        //    ThreadB wartet auf ThreadA, ThreadC wartet auf ThreadB
+        taskB.setJoinFor(threadA);
+        taskC.setJoinFor(threadB);
+
+        // 4) Starte alle Threads
+        LOG.info("Main: Starte ThreadA, ThreadB, ThreadC ...");
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+        // 5) Unterbrechungs-Demo: Main-Thread wartet kurz und unterbricht dann ThreadB
+        Thread.sleep(1000);
+        LOG.info("Main: Unterbreche ThreadB!");
+        threadB.interrupt();
+
+        // 6) Warte auf das Ende aller Threads
+        threadA.join();
+        threadB.join();
+        threadC.join();
+
+        LOG.info("Main: Alle Threads sind fertig. Ende des Programms.");
+    }
 }
--- ad_ex_nan/src/main/java/ch/hslu/sw04/joins/JoinAndSleepTask.java
+++ ad_ex_nan/src/main/java/ch/hslu/sw04/joins/JoinAndSleepTask.java
@@ -1,11 +1,69 @@
 package ch.hslu.sw04.joins;
 
-public class JoinAndSleepTask implements Runnable{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Task for Joining Threads.
+ *
+ * @author Nicola Anghileri - nicola.anghileri@stud.hslu.ch
+ * @version 17.03.2025
+ */
+public class JoinAndSleepTask implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JoinAndSleepTask.class);
+
+    private final String taskName;
+    private Thread joinFor;      // Falls dieser Thread != null ist, wird darauf gewartet.
+    private final int sleepTime; // Zeit in ms, die geschlafen wird.
+
+    /**
+     * Erzeugt einen Task mit Namen und Schlafzeit.
+     *
+     * @param taskName Name des Tasks
+     * @param sleepTime Zeit in ms, die geschlafen wird
+     */
+    public JoinAndSleepTask(final String taskName, final int sleepTime) {
+        this.taskName = taskName;
+        this.sleepTime = sleepTime;
+    }
+
+    /**
+     * Setzt den Thread, auf den gewartet werden soll.
+     *
+     * @param joinFor Thread, auf den gewartet wird
+     */
+    public void setJoinFor(Thread joinFor) {
+        this.joinFor = joinFor;
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        LOG.info("Task '{}' gestartet.", taskName);
+
+        // 1) Join-Teil (optional, nur wenn joinFor != null)
+        if (joinFor != null) {
+            try {
+                LOG.info("Task '{}' wartet auf Thread '{}'.", taskName, joinFor.getName());
+                joinFor.join();
+                LOG.info("Task '{}' hat das Warten auf '{}' beendet.", taskName, joinFor.getName());
+            } catch (InterruptedException ex) {
+                LOG.warn("Task '{}' wurde unterbrochen, während er auf '{}' wartete!", taskName, joinFor.getName());
+                return; // Abbruch des Tasks
+            }
+        }
+
+        // 2) Sleep-Teil
+        try {
+            LOG.info("Task '{}' schläft jetzt für {} ms.", taskName, sleepTime);
+            Thread.sleep(sleepTime);
+            LOG.info("Task '{}' ist aufgewacht.", taskName);
+        } catch (InterruptedException ex) {
+            LOG.warn("Task '{}' wurde während des Schlafens unterbrochen!", taskName);
+            return; // Abbruch des Tasks
+        }
+
+        LOG.info("Task '{}' beendet sich regulär.", taskName);
     }
-    
+
 }
